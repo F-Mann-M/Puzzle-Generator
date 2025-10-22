1a742ee2d8b5070d30add13ea1056c9892ffb330
Friedemann Muecket <friedemann@muecket.de>
2025-10-22 17:15:41 +0200

    first commit

---
+++ app/models/puzzles.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, func, DateTime
+from app.database import Base
+
+class Puzzle(Base):
+    __tablename__ = "puzzles"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    model = Column(String, nullable=False)
+    enemy_count = Column(Integer) # since there is a unit table this makes no sense, or?
+    player_unit_count = Column(Integer, nullable=False) # same gose for player_units
+    game_mode = Column(String, nullable=False)
+    node_count = Column(Integer, nullable=False)
+    coins = Column(Integer, nullable=False) # coins equal turns
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
