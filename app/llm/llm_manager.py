d51ecca34e0d9811221e7211bf31589af9b8b406
Friedemann <79202584+F-Mann-M@users.noreply.github.com>
2026-01-17 16:56:39 +0100

    Remove unused import and add commented LLM selector
    Removed an unused import from llm_manager.py and added a commented-out function for selecting LLMs, possibly for future use. Also deleted the puzzle.db-wal file.

--- app/llm/llm_manager.py
+++ app/llm/llm_manager.py
@@ -1,5 +1,4 @@
 import logging
-from utils.logger_config import configure_logging
 
 from app.llm.openai_client import OpenAIClient
 from app.llm.gemini_client import GeminiClient
@@ -10,3 +9,11 @@ def get_llm(model_name: str):
         return OpenAIClient(model_name)
     elif model_name.startswith("gemini"):
         return GeminiClient(model_name)
+
+
+# def get_lang_graph_llm(model_name: str):
+#     """ Select model based on name"""
+#     if model_name.startswith("gpt"):
+#         return OpenAIClient(model_name)
+#     elif model_name.startswith("gemini"):
+#         return GeminiClient(model_name)
